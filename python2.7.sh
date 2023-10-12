f0ceda55a9cb649349e6ce18a0cf6938af3c2665
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-10-12 08:55:14 +0800

    Create python2.7.sh

---
+++ python2.7.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+RED="\033[31m"
+BLUE="\033[36m"
+PLAIN='\033[0m'
+
+colorEcho() {
+    echo -e "${1}${@:2}${PLAIN}"
+}
+
+checkSystem() {
+    result=$(id | awk '{print $1}')
+    if [[ $result != "uid=0(root)" ]]; then
+        result=$(id | awk '{print $1}')
+	if [[ $result != "用户id=0(root)" ]]; then
+        colorEcho $RED " 请以root身份执行该脚本"
+        exit 1
+	fi
+    fi
+
+    res=`which yum 2>/dev/null`
+    if [[ "$?" != "0" ]]; then
+        res=`which apt 2>/dev/null`
+        if [[ "$?" != "0" ]]; then
+            colorEcho $RED " 不受支持的Linux系统"
+            exit 1
+        fi
+        PMT="apt"
+        CMD_INSTALL="apt install -y"
+	      CMD_AUTOREMOVE="apt autoremove -y"
+	      CMD_UPGRADE="apt update"
+    else
+        PMT="yum"
+        CMD_INSTALL="yum install -y"
+	      CMD_UPGRADE=""
+    fi
+    res=`which systemctl 2>/dev/null`
+    if [[ "$?" != "0" ]]; then
+        colorEcho $RED " 系统版本过低，请升级到最新版本"
+        exit 1
+    fi
+}
+
+archAffix() {
+    case "$(uname -m)" in
+        x86_64|amd64)
+            ARCH="amd64"
+        ;;
+        armv8|aarch64)
+            ARCH="arm64"
+        ;;
+        *)
+            colorEcho $RED " 不支持的CPU架构！"
+            exit 1
+        ;;
+    esac
+
+	return 0
+}
+
+Install() {
+  	checkSystem
+   	archAffix
+    CMD_UPGRADE
+    CMD_INSTALL python2.7
+	  wget -N --no-check-certificate https://raw.githubusercontent.com/Slotheve/backup/main/pip2.py
+	  python2.7 pip2.py && rm pip2.py >/dev/null
+    colorEcho $BLUE " 安装完成"
+}
+
+checkSystem
+Install
