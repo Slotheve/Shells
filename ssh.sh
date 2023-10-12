2ebd6509d24ab4d68e02dafa320d89b75075022d
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-10-12 22:32:06 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -37,10 +37,25 @@ checkSystem() {
 }
 
 Install() {
-	sed -i '/Port 22/d' /etc/ssh/sshd_config 
+  read -p $'请输入需要设置的端口:' PORT
+  echo $((${PORT}+0)) &>/dev/null
+  if [[ $? -eq 0 ]]; then
+      if [[ ${PORT} -ge 1 ]] && [[ ${PORT} -le 65535 ]]; then
+          colorEcho $BLUE "SSH端口: ${PORT}"
+          echo ""
+      else
+          colorEcho $RED "输入错误, 请输入正确的端口。"
+          echo ""
+      fi
+  else
+      colorEcho $RED "输入错误, 请输入数字。"
+      echo ""
+      exit 1
+  fi
+  sed -i '/Port 22/d' /etc/ssh/sshd_config 
   sed -i '/PubkeyAuthentication/d' /etc/ssh/sshd_config 
   sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config 
-  echo "Port 2222" >> /etc/ssh/sshd_config 
+  echo "Port $PORT" >> /etc/ssh/sshd_config 
   echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config 
   echo "PasswordAuthentication no" >> /etc/ssh/sshd_config 
   systemctl restart sshd
