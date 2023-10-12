f1f15930609ab534b6e1eae8f4f9452408b20f0f
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-10-12 09:02:27 +0800

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+RED="\033[31m"
+BLUE="\033[36m"
+PLAIN='\033[0m'
+
+colorEcho() {
+    echo -e "${1}${@:2}${PLAIN}"
+}
+
+checkSystem() {
+    result=$(id | awk '{print $1}')
+    if [[ $result != "uid=0(root)" ]]; then
+        result=$(id | awk '{print $1}')
+	if [[ $result != "用户id=0(root)" ]]; then
+        colorEcho $RED " 请以root身份执行该脚本"
+        exit 1
+	fi
+    fi
+
+    res=`which yum 2>/dev/null`
+    if [[ "$?" != "0" ]]; then
+        res=`which apt 2>/dev/null`
+        if [[ "$?" != "0" ]]; then
+          res=`which apk 2>/dev/null`
+          if [[ "$?" != "0" ]]; then
+            colorEcho $RED " 不受支持的Linux系统"
+            exit 1
+          fi
+        fi
+    fi
+    res=`which systemctl 2>/dev/null`
+    if [[ "$?" != "0" ]]; then
+        colorEcho $RED " 系统版本过低，请升级到最新版本"
+        exit 1
+    fi
+}
+
+Install() {
+	sed -i '/Port 22/d' /etc/ssh/sshd_config 
+  sed -i '/PubkeyAuthentication/d' /etc/ssh/sshd_config 
+  sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config 
+  echo "Port 2222" >> /etc/ssh/sshd_config 
+  echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config 
+  echo "PasswordAuthentication no" >> /etc/ssh/sshd_config 
+  systemctl restart sshd
+  colorEcho $BLUE " 安装完成"
+}
+
+checkSystem
+Install
